d91f6a969f4a9bea54b9447d7d053063da0cae77
Mack Cooper <mackcooper1408@gmail.com>
2024-08-10 11:40:10 -0700

    change delayLine read to use hermite interpolation

--- Source/DelayLine.cpp
+++ Source/DelayLine.cpp
@@ -15,7 +15,7 @@ void DelayLine::setMaximumDelayInSamples(int maxLengthInSamples)
 {
     jassert(maxLengthInSamples > 0);
     
-    int paddedLength = maxLengthInSamples + 1;
+    int paddedLength = maxLengthInSamples + 2;
     
     if (bufferLength < paddedLength) {
         bufferLength = paddedLength;
@@ -45,7 +45,7 @@ void DelayLine::write(float input) noexcept
     buffer[size_t(writeIndex)] = input;
 }
 
-float DelayLine::read(float delayInSamples) const noexcept
+/* float DelayLine::read(float delayInSamples) const noexcept
 {
     jassert(delayInSamples >= 0.0f);
     jassert(delayInSamples <= bufferLength - 1.0f);
@@ -57,4 +57,48 @@ float DelayLine::read(float delayInSamples) const noexcept
     }
     
     return buffer[size_t(readIndex)];
+}
+*/
+
+float DelayLine::read(float delayInSamples) const noexcept
+{
+    jassert(delayInSamples >= 1.0f);
+    jassert(delayInSamples <= bufferLength - 2.0f);
+    
+    int integerDelay = int(delayInSamples);
+    
+    int readIndexA = writeIndex - integerDelay + 1;
+    int readIndexB = readIndexA - 1;
+    int readIndexC = readIndexA - 2;
+    int readIndexD = readIndexA - 3;
+
+    if (readIndexD < 0) {
+        readIndexD += bufferLength;
+        if (readIndexC < 0) {
+            readIndexC += bufferLength;
+            if (readIndexB < 0) {
+                readIndexB += bufferLength;
+                if (readIndexA < 0) {
+                    readIndexA += bufferLength;
+                }
+            }
+        }
+    }
+    
+    float sampleA = buffer[size_t(readIndexA)];
+    float sampleB = buffer[size_t(readIndexB)];
+    float sampleC = buffer[size_t(readIndexC)];
+    float sampleD = buffer[size_t(readIndexD)];
+    
+    float fraction = delayInSamples - float(integerDelay);
+    float slope0 = (sampleC - sampleA) * 0.5f;
+    float slope1 = (sampleD - sampleB) * 0.5f;
+    float v = sampleB - sampleC;
+    float w = slope0 + v;
+    float a = w + v + slope1;
+    float b = w + a;
+    float stage1 = a * fraction - b;
+    float stage2 = stage1 * fraction + slope0;
+    
+    return stage2 * fraction + sampleB;
 }
