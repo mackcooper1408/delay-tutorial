9c1a34ee272364d988e97c1778d03c02af491436
Mack Cooper <mackcooper1408@gmail.com>
2024-08-20 21:58:36 -0700

    update config and run release builds

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -59,7 +59,7 @@
  #define JucePlugin_ManufacturerCode       0x4d616e75
 #endif
 #ifndef  JucePlugin_PluginCode
- #define JucePlugin_PluginCode             0x52307168
+ #define JucePlugin_PluginCode             0x446c6179
 #endif
 #ifndef  JucePlugin_IsSynth
  #define JucePlugin_IsSynth                0
